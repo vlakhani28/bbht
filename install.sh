e5cc317b8081a098c2ad5ccd3b20e0b16a5f0ba5
Vaibhav Lakhani <vlakhani28@gmail.com>
2021-04-18 17:15:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,13 @@ sudo apt-get install -y rename
 sudo apt-get install -y xargs
 sudo apt-get install -y screen
 
+#Install Golang.
+echo -e "Installing Go-Lang";
+wget -q -O - https://git.io/vQhTU | bash
+echo "source ~/.bashrc" >> ~/.bash_profile
+source ~/.bash_profile
+echo -e " ";
+
 echo "installing bash_profile aliases from recon_profile"
 git clone https://github.com/nahamsec/recon_profile.git
 cd recon_profile
